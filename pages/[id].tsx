b57f45ce3d954032f426057a859dba6719f5bea3
kenmueller <kenmueller0@gmail.com>
2020-12-14 21:19:11 -0800

    connects to backend

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,9 +1,13 @@
 import { useState, useEffect, Fragment } from 'react'
 import { NextPage } from 'next'
-import Router from 'next/router'
+import { useRouter } from 'next/router'
+import Head from 'next/head'
+import { io as IO } from 'socket.io-client'
+import { toast } from 'react-toastify'
 
 import { GAME_COLUMNS, GAME_ROWS } from 'models/Game'
 import User from 'models/User'
+import getId from 'lib/getId'
 
 import styles from 'styles/GamePage.module.scss'
 
@@ -11,14 +15,30 @@ const ROWS = Array.from(new Array(GAME_ROWS))
 const COLUMNS = Array.from(new Array(GAME_COLUMNS))
 
 const GamePage: NextPage = () => {
+	const router = useRouter()
+	const gameId = router.query.id as string | undefined
+	
 	const [users, setUsers] = useState<User[] | null>(null)
 	
 	useEffect(() => {
+		if (!gameId)
+			return
 		
-	}, [])
+		const io = IO(process.env.NEXT_PUBLIC_API_BASE_URL, {
+			query: { id: getId(), game: gameId }
+		})
+		
+		io.on('not-found', () => {
+			router.push('/')
+			toast.error('Game not found')
+		})
+	}, [router, gameId])
 	
 	return (
 		<div className={styles.root}>
+			<Head>
+				<link key="api-preconnect" rel="preconnect" href={process.env.NEXT_PUBLIC_API_BASE_URL} />
+			</Head>
 			<div className={styles.grid}>
 				{ROWS.map((_row, row) => (
 					<Fragment key={row}>
